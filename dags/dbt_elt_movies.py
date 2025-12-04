c3ce4b6abf62d6616b238f708fa5d104fa10f6d6
Benita <benita.isaac@gmail.com>
2025-12-04 15:47:11 -0800

    added dag for elt

---
+++ dags/dbt_elt_movies.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "data226-group",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+}
+
+DBT_DIR = "/opt/airflow/dbt/imdb_analytics"
+
+with DAG(
+    dag_id="dbt_imdb_elt",
+    default_args=default_args,
+    description="Run dbt ELT models for IMDb project",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["dbt", "elt", "imdb"],
+) as dag:
+
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command=f"cd {DBT_DIR} && dbt run"
+    )
+
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command=f"cd {DBT_DIR} && dbt test"
+    )
+
+    dbt_run >> dbt_test
